27ac289ae7e9bb12c5350c0ac544c343c84fac17
pkukla <pkukla@criterion-em3.statelibrary.local>
2023-10-27 22:53:18 +0000

    note to self

--- nopox/sloapp-home.php
+++ nopox/sloapp-home.php
@@ -6,4 +6,5 @@
 -->
 <?php
 echo "<p>This is the sloapp-home.php content.</p><p>This will be the GUI admin console for the application that will replace REPOX in the Ohio Digital Network's preparation and processing of data for DPLA.</p>";
+echo "<br><br><p>Note to self:  Add to this page tallies of... providers, total records, IIIF records, contributed sets</p>";
 ?>
