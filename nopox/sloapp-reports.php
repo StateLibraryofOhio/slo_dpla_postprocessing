13cb501915ddd257601dc3c9ffe9264000387461
Peter Kukla <pkukla@library.ohio.gov>
2023-11-17 19:21:04 +0000

    Add a few reports

--- nopox/sloapp-reports.php
+++ nopox/sloapp-reports.php
@@ -1,3 +1,10 @@
 <?php
-echo "<p>This is the sloapp-reports.php content.</p><p>It might be nice to have stuff about:</p><ul><li>Lists of collections by edm:rights</li><li>Lists of collections eligible for IIIF participation, but not participating</li><li>Lists of providers using a given platform for their OAI-PMH server (e.g. CONTENTdm, BePress, Islandora)</li></ul>";
+echo "<p>This is the sloapp-reports.php content.</p>";
+echo "";
+echo "<ul>
+          <li><a href='/?action=list-all-rights'>Lists of collections by edm:rights</a></li>
+          <li><a href='/?action=count-sets-by-org'>Counts of sets submitted by each provider</a></li>
+          <li>Lists of collections eligible for IIIF participation, but not participating</li>
+          <li>Lists of providers using a given platform for their OAI-PMH server (e.g. CONTENTdm, BePress, Islandora)</li>
+      </ul>";
  ?>
