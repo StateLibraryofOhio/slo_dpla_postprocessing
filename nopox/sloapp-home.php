de0d4179eaa36f28bd685d768f8ac010de42dc39
Peter Kukla <pkukla@library.ohio.gov>
2023-11-18 02:08:48 +0000

    Move edm:rights link to reports area

--- nopox/sloapp-home.php
+++ nopox/sloapp-home.php
@@ -6,6 +6,4 @@
 -->
 <?php
 echo "<p>This is the sloapp-home.php content.</p><p>This will be the GUI admin console for the application that will replace REPOX in the Ohio Digital Network's preparation and processing of data for DPLA.</p>";
-echo "<br><p><a href='/?action=list-all-rights'>Find a set containing records with a specific edm:rights URI</a></p>";
-echo "<br><br><p>Note to self:  Add to this page tallies of... providers, total records, IIIF records, contributed sets</p>";
 ?>
