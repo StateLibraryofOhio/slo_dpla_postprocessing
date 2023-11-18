e5894d65bd3b38fb5d9fb771eecee6fd97c6b151
Peter Kukla <fruviad@yahoo.com>
2023-11-18 20:08:49 +0000

    Enable link to the new report

--- nopox/sloapp-reports.php
+++ nopox/sloapp-reports.php
@@ -5,6 +5,6 @@ echo "<ul>
           <li><a href='/?action=list-all-rights'>Lists of collections by edm:rights</a></li>
           <li><a href='/?action=count-sets-by-org'>Counts of sets submitted by each provider</a></li>
           <li>Lists of collections eligible for IIIF participation, but not participating</li>
-          <li>Lists of providers using a given platform for their OAI-PMH server (e.g. CONTENTdm, BePress, Islandora)</li>
+          <li><a href='/?action=list-providers-by-cms'>Lists of providers using a given platform for their OAI-PMH server</a> (e.g. CONTENTdm, BePress, Islandora)</li>
       </ul>";
  ?>
