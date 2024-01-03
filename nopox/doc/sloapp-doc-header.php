5d2fab683229a69f565ae202c4fc65a043391c69
Peter Kukla <pkukla@library.ohio.gov>
2024-01-03 18:15:59 +0000

    Fix "Process Overview" link

--- nopox/doc/sloapp-doc-header.php
+++ nopox/doc/sloapp-doc-header.php
@@ -1,6 +1,6 @@
 <table class="sloapp-doc-header-table" align="center">
 <tr class="sloapp-doc-header-row">
-  <td class="sloapp-doc-header-td"><a href=".?action=notYet">Process Overview</a> || </td>
+  <td class="sloapp-doc-header-td"><a href=".?action=doc-base">Process Overview</a> || </td>
   <td class="sloapp-doc-header-td"><a href=".?action=doc-commandline">Command Line</a> || </td>
   <td class="sloapp-doc-header-td"><a href=".?action=doc-webui">Web UI</a></td>
 <!--  <td class="sloapp-doc-header-td"><a href=".?action=list-harvests">Harvests</a></td>
