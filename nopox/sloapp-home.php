9bd607034b89dd251f0dbb2f2ac7bab4d455afee
Peter Kukla <pkukla@library.ohio.gov>
2024-01-17 18:49:54 +0000

    Change text of link

--- nopox/sloapp-home.php
+++ nopox/sloapp-home.php
@@ -9,7 +9,7 @@ echo "<p>This a console for the application that will replace REPOX in the Ohio 
 
 <p>This web interface only gives one the ability to view what happening in the system.  It cannot be used to make changes.</p>
 
-<p>If you're unsure what do to next, you might look at <a href='.?action=doc-base'>the little documentation</a> there is to be had.</p>
+<p>If you're unsure what do to next, you might look at <a href='doc/.?action=doc-base'>the little documentation</a> there is to be had.</p>
 ";
 
 ?>
